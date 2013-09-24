203b328abfaa6bcf760936427ce8002e24eec55e
yong-uk choe <yonguk.choe@plus-i.co.kr>
2013-09-24 16:39:29 +0900

    made result(interface) string

--- mns.pl
+++ mns.pl
@@ -8,17 +8,31 @@ $debian_script_path = "/etc/network";
 
 foreach $redhat_ifcfg_file (@redhat_ifcfgs) {
   open(FP, $redhat_ifcfg_file);
+  $result = "";
   while(<FP>) {
     if (!/^#/ && !/^$/) {
       chop;
       if (/.*=.*/) {
         ($key, $value) = split(/=/, $_, 2);
-        #print $key . " - " . $value . "\n";
         $redhat_ifcfg{$key} = $value;
       }
     }
   } # of while FP
-  print $redhat_ifcfg{'DEVICE'} . "\n";
-
+  if (lc($redhat_ifcfg{'ONBOOT'}) eq "yes") {
+    $result =           "auto $redhat_ifcfg{'DEVICE'}\n";
+    if ($redhat_ifcfg{'DEVICE'} eq 'lo') {
+      # interface : lo
+      $result = $result . "iface lo inet loopback\n"; 
+    } else {
+      # interface : eth*
+      if (lc($redhat_ifcfg{'BOOTPROTO'}) eq "static") {
+        $result = $result . "iface $redhat_ifcfg{'DEVICE'}\n"; 
+        $result = $result . "  address $redhat_ifcfg{'IPADDR'}\n";
+        $result = $result . "  netmask $redhat_ifcfg{'NETMASK'}\n";
+      }
+    } 
+    $result = $result . "\n";
+    print $result;
+  }
 } # of for ifcfg_files
 
