37b12316ad9ee7e5adf110b1e743a55c69e257b9
yong-uk choe <yonguk.choe@plus-i.co.kr>
2013-09-27 10:18:07 +0900

    ifcfg_path 오타 수정. DEBUG OFF

--- mns.pl
+++ mns.pl
@@ -1,10 +1,19 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl 
 
-#$redhat_script_path = "/etc/sysconfig/network-script";
-$redhat_script_path = "./network-scripts";
-$debian_script_path = "/etc/network";
+use constant { TRUE => 1, FALSE => 0 };
+
+$DEBUG_MODE = FALSE;
+
+if ($DEBUG_MODE == TRUE) {
+  $redhat_script_path = "./network-scripts";
+  $debian_script_path = "./network-scripts";
+} else {
+  $redhat_script_path = "/etc/sysconfig/network-scripts";
+  $debian_script_path = "/etc/network";
+}
 
 @redhat_ifcfgs = <$redhat_script_path/ifcfg-*>;
+open(INTERFACE_FP, ">", "$debian_script_path/interface");
 
 foreach $redhat_ifcfg_file (@redhat_ifcfgs) {
   open(FP, $redhat_ifcfg_file);
@@ -26,13 +35,21 @@ foreach $redhat_ifcfg_file (@redhat_ifcfgs) {
     } else {
       # interface : eth*
       if (lc($redhat_ifcfg{'BOOTPROTO'}) eq "static") {
+        # static ip address 
         $result = $result . "iface $redhat_ifcfg{'DEVICE'}\n"; 
         $result = $result . "  address $redhat_ifcfg{'IPADDR'}\n";
         $result = $result . "  netmask $redhat_ifcfg{'NETMASK'}\n";
-      }
+      } elsif (lc($redhat_ifcfg{'BOOTPROTO'}) eq "dhcp") {
+        # dhcp 
+        $result = $result . "iface $redhat_ifcfg{'DEVICE'} inet dhcp\n";
+      } elsif (lc($redhat_ifcfg{'BOOTPROTO'}) eq "dialup") {
+        # pppoe
+        # pass 
+      } 
     } 
     $result = $result . "\n";
-    print $result;
+    print INTERFACE_FP $result;
   }
 } # of for ifcfg_files
+close(INTERFACE_FP);
 
